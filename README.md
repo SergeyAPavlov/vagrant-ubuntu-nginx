4631543a5a5c8967c6cf39a0257c8a42818518a4
Paul Underwood <paul.und@gmail.com>
2016-07-10 15:58:45 +0100

    Created the vagrant project.

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# vagrant-ubuntu-nginx
-Default setup for Ubuntu With NGINX PHP MySQL
+# Default Setup for Ubuntu NGINX MySQL On Vagrant
+
+This project automates the setup of a LEMP development environment.
+
+## Requirements
+
+* [VirtualBox](https://www.virtualbox.org)
+
+* [Vagrant](http://vagrantup.com)
+
+* [Cygwin](https://www.cygwin.com/) or any other ssh-capable terminal shell for the `vagrant ssh` command
+
+## Recommended Workflow
+
+The recommended workflow is
+
+* edit files in the host computer
+
+* run within the virtual machine
+
+Your home folder is synced to `/home/vagrant/code` on the guest.
+
+## Database
+* For mysql the default user is root: `mysql -u root`
+
+
+## Virtual Machine Management
+
+When done just log out with and suspend the virtual machine
+
+    host $ vagrant suspend
+
+then, resume to hack again
+
+    host $ vagrant resume
+
+Run
+
+    host $ vagrant halt
+
+to shutdown the virtual machine, and
+
+    host $ vagrant up
+
+to boot it again.
+
+You can find out the state of a virtual machine anytime by invoking
+
+    host $ vagrant status
+
+Finally, to completely wipe the virtual machine from the disk **destroying all its contents**:
+
+    host $ vagrant destroy # DANGER: all is gone
+
+Please check the [Vagrant documentation](http://docs.vagrantup.com/v2/) for more information on Vagrant.
---
+++ vagrant/provision.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+export DEBIAN_FRONTEND=noninteractive
+
+sudo aptitude update -q
+
+# Force a blank root password for mysql
+echo "mysql-server mysql-server/root_password password " | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password " | debconf-set-selections
+
+# Install mysql, nginx, php5-fpm
+sudo aptitude install -q -y -f mysql-server mysql-client nginx php5-fpm
+
+# Install commonly used php packages
+sudo aptitude install -q -y -f php5-mysql php5-curl php5-gd php-pear php5-imap php5-mcrypt php5-mssql php5-odbc unixodbc php5-ldap php5-pgsql
+
+# Install Git
+sudo apt-get install git -y > /dev/null
+
+sudo rm /etc/nginx/sites-available/default
+sudo touch /etc/nginx/sites-available/default
+
+sudo cat >> /etc/nginx/sites-available/default <<'EOF'
+server {
+  listen   80;
+
+  root /home/vagrant/code/public;
+  index index.php index.html index.htm;
+
+  # Make site accessible from http://localhost/
+  server_name server_domain_or_IP;
+
+  location / {
+    # First attempt to serve request as file, then
+    # as directory, then fall back to index.html
+    try_files $uri $uri/ /index.php?$query_string;
+  }
+
+  location /doc/ {
+    alias /usr/share/doc/;
+    autoindex on;
+    allow 127.0.0.1;
+    deny all;
+  }
+
+  # redirect server error pages to the static page /50x.html
+  #
+  error_page 500 502 503 504 /50x.html;
+  location = /50x.html {
+    root /usr/share/nginx/html;
+  }
+
+  # pass the PHP scripts to FastCGI server listening on /tmp/php5-fpm.sock
+  #
+  location ~ \.php$ {
+        try_files $uri /index.php =404;
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_pass unix:/var/run/php5-fpm.sock;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        include fastcgi_params;
+  }
+
+  ### phpMyAdmin ###
+  location /phpmyadmin {
+    root /usr/share/;
+    index index.php index.html index.htm;
+    location ~ ^/phpmyadmin/(.+\.php)$ {
+      client_max_body_size 4M;
+      client_body_buffer_size 128k;
+      try_files $uri =404;
+      root /usr/share/;
+
+      # Point it to the fpm socket;
+      fastcgi_pass unix:/var/run/php5-fpm.sock;
+      fastcgi_index index.php;
+      fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+      include /etc/nginx/fastcgi_params;
+    }
+
+    location ~* ^/phpmyadmin/(.+\.(jpg|jpeg|gif|css|png|js|ico|html|xml|txt)) {
+      root /usr/share/;
+    }
+  }
+  location /phpMyAdmin {
+    rewrite ^/* /phpmyadmin last;
+  }
+  ### phpMyAdmin ###
+}
+EOF
+
+sudo touch /usr/share/nginx/html/info.php
+sudo cat >> /usr/share/nginx/html/info.php <<'EOF'
+<?php phpinfo(); ?>
+EOF
+
+# Install phpmyadmin
+sudo aptitude install -q -y -f phpmyadmin
+
+# Install Composer
+sudo apt-get install -y php5-cli curl > /dev/null
+curl -Ss https://getcomposer.org/installer | php > /dev/null
+sudo mv composer.phar /usr/bin/composer
+
+sudo service nginx restart
+
+sudo service php5-fpm restart
